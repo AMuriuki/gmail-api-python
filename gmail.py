473a66681e4a2fa7f3d2cc63361d8a538e07b049
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-09-08 15:11:40 +0300

    authenticate with Gmail API

--- gmail.py
+++ gmail.py
@@ -23,3 +23,31 @@ from decouple import config
 # Request all access (permission to read/send/receive emails & manage inbox and more)
 SCOPES = ['https://mail.google.com/']
 email_address = config('email_address')
+
+
+def gmail_authenticate():
+    creds = None
+    # token.pickle stores user access and refreshes tokens
+    # this file is created automatically
+    # after the authorization flow completes for the 1st time
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+
+    # if no valid credentials available, allow user to log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # save credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+    return build('gmail', 'v1', credentials=creds)
+
+
+# get the Gmail API service
+service = gmail_authenticate()
