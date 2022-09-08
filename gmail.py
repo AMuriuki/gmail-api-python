a708294cd189f0ea6db9c5bdf76a1612f9446aa1
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-09-08 14:46:58 +0300

    import necessary modules

---
+++ gmail.py
@@ -0,0 +1,25 @@
+import imp
+import os
+import pickle
+
+# Gmail API utils
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# for encoding/decoding messages in base64
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+
+# for dealing with attachment MIME types
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from mimetypes import guess_type as guess_mime_type
+
+from decouple import config
+
+# Request all access (permission to read/send/receive emails & manage inbox and more)
+SCOPES = ['https://mail.google.com/']
+email_address = config('email_address')
